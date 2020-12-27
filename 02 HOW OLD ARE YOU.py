c49f80232cc5f6b2e87f868bc26515ef08c0eecc
GodnoPyt <timakinvlad@gmail.com>
2020-12-27 15:30:27 +0300

    Create 02 HOW OLD ARE YOU.py

---
+++ 02 HOW OLD ARE YOU.py
@@ -0,0 +1,10 @@
+print("Hello, user! What is your name?")
+
+user_name = input("What is your name?   ")
+
+print('Hello', user_name)
+
+user_year = int(input("Bro , when you was born?   "))
+print("nice year, ", user_year)
+
+print(2020-user_year)
