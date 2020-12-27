adf68911a3ec435a310a78a65f941fc25b2f9f05
GodnoPyt <timakinvlad@gmail.com>
2020-12-27 16:24:49 +0300

    Update 02 HOW OLD ARE YOU.py

--- 02 HOW OLD ARE YOU.py
+++ 02 HOW OLD ARE YOU.py
@@ -2,9 +2,13 @@ print("Hello, user! What is your name?")
 
 user_name = input("What is your name?   ")
 
-print('Hello', user_name)
+print('Hello', user_name, ".In your name ", len(user_name), "Letters")
 
 user_year = int(input("Bro , when you was born?   "))
-print("nice year, ", user_year)
+if len(str(user_year)) == 4:
+ print("Nice year, ", user_year)
+ print(2020-user_year)
+else:
+ print("You make a little mistake")
 
 print(2020-user_year)
