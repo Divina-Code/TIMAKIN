e4654eb3120e1354b0eb34dae7f9f97dbcc82974
GodnoPyt <timakinvlad@gmail.com>
2020-12-28 14:36:05 +0300

    Create 05 Homework.py

---
+++ 05 Homework.py
@@ -0,0 +1,16 @@
+isGuessed = False
+input("Hello, Let's play the game guess the number ")
+while isGuessed != True:
+    answer = ("Guess the number")
+    computer_number = input(answer)
+    if computer_number == "35":
+        print('Well done')
+        isGuessed = True
+    elif answer => "35":
+        print('Try again')
+    else:
+        print('Almost...')
+
+
+
+
