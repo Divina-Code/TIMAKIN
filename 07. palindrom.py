7c45be8a5ef74e6a5a695c3cc772f2490bf0d176
GodnoPyt <timakinvlad@gmail.com>
2020-12-29 18:33:15 +0300

    Update 07. palindrom.py

--- 07. palindrom.py
+++ 07. palindrom.py
@@ -1,7 +1,7 @@
 print("Привет!")
 palindrom = input("Напиши палиндром ").lower()
 palEdrom = (palindrom[::-1])
- if palindrom == palEdrom :
+if palindrom == palEdrom :
      print("Ага, это палиндром")
- else:
-     print("Неа , это не палиндром")
+else:
+  print("Неа , это не палиндром")
